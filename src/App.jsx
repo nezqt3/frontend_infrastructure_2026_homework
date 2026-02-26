19796aa7ca12d36690a5bac02e62f19d4d5d50c0
Denis <dalekseenko8@gmail.com>
2026-02-26 09:02:14 +0300

    have added base structure of project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import MainScreen from './components/MainScreen'
+
+function App() {
+
+  return (
+    <div>
+      <MainScreen/>
+    </div>
+  )
+}
+
+export default App
